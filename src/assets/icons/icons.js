a691ce787e1e9dcb200c6f4b68d2b93584cf43b2
KarimASoliman3 <solimankarim490@gmail.com>
2026-05-06 03:03:40 +0300

    add styles and icons img

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,24 @@
+import Dashboard from '../icons/layout-dashboard.svg';
+import Logout from '../icons/logout.svg'; 
+import Users from '../icons/users.svg'; 
+import Dark from '../icons/moon.svg'; 
+import Light from '../icons/sun.svg'; 
+import Products from '../icons/shopping-bag.svg'; 
+import Chart from '../icons/chart-line.svg'; 
+import Bell from '../icons/bell.svg'; 
+import Inbox from '../icons/inbox.svg'; 
+
+
+
+export const icons = {
+  dashboard: Dashboard,
+  logout: Logout,
+  users: Users,
+  dark: Dark,
+  light: Light,
+  products: Products,
+  chart: Chart,
+  bell: Bell,
+  inbox: Inbox,
+
+};
